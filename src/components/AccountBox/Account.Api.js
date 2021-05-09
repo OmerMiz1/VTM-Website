a20881021a30edd43c18ef38c5b8b62c0ed68d41
Omer Mizrachi <omermizra@gmail.com>
2021-05-09 18:57:18 +0300

    Added login & register functions

--- src/components/AccountBox/Account.Api.js
+++ src/components/AccountBox/Account.Api.js
@@ -1,14 +1,44 @@
-
+import {Auth} from 'aws-amplify';
 const AccountApi = () => {
   
-  //MOCK ->post to the server (signup) 
-  const PostSingupForm = (data) => {
-    console.log('sign up:' ,data);
+  const PostSingupForm = async (data) => {
+    console.log('sign up:' ,data); //DELETEME
+    data.preventDefault();
+
+    try {
+      await Auth.signIn(userName, password)
+        .then(user => {
+          alert("Logged in");
+          props.login(user);
+          history.push("/myHome");
+          console.log(user.getUsername()); //DELETEME
+        });
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
-  //MOCK ->post to the server (signup) 
-  const PostLoginForm = (data) => {
-    console.log('login:' ,data);
+  const PostLoginForm = async (data) => {
+    console.log('login:' ,data);  // DELETEME
+
+    data.preventDefault();
+        let username = email;
+        try {
+            const { user } = await Auth.signUp({
+                username,
+                password,
+                attributes: {
+                    email,
+                },
+            });
+            console.log(user);
+        } catch (error) {
+            console.log('error signing up:', error);
+            return;
+        }
+        console.log("Register succses email: " + email + "password: " + password);  // DELETEME
+        props.login(email);  // TODO signin with Auth.signIn and update userObject (instead of email)
+        history.push("/myHome")
   }
 
   return {PostSingupForm, PostLoginForm};
