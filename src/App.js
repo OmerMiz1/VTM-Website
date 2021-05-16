955d1ca64c841ba88f8bb442e3629c11396cd1a1
Omer Mizrachi <omermizra@gmail.com>
2021-05-16 15:34:55 +0300

    Account API: signup, login, logout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+
 //global style
 import {ThemeProvider} from 'styled-components';
 import { GlobalStyle } from './utils/globalStyle/globalStyle.style';
@@ -11,26 +12,30 @@ import HomePage from './pages/HomePage'
 import AccessAccountPage from './pages/AccessAccountPage';
 import MyHomePage from './pages/MyHomePage';
 
+// Contexts
+import { UserContext } from './utils/context/UserContext';
 
 function App() {
+  const [user, setUser] = useState(null);
 
   return (
     <Router>
       <ThemeProvider theme= {Theme}>
-      <GlobalStyle/>
-        <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route exact path='/home' component={HomePage}/>
-          <Route exact path='/about' component={HomePage}/>
-          <Route exact path='/getstart' component={HomePage}/>
-          <Route exact path='/download' component={HomePage}/>
-          <Route path='/myHome/:page' component={MyHomePage}/> 
-          <Route exact path='/access/:action' component={AccessAccountPage}/>
-          <Route exact path="/notFound404" component={NotFound404}/>
-          <Redirect to="/notFound404" />
-        </Switch>
+        <GlobalStyle/>
+        <UserContext.Provider value={{user, setUser}}>
+          <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/home' component={HomePage}/>
+            <Route exact path='/about' component={HomePage}/>
+            <Route exact path='/getstart' component={HomePage}/>
+            <Route exact path='/download' component={HomePage}/>
+            <Route path='/myHome/:page' component={MyHomePage}/> 
+            <Route exact path='/access/:action' component={AccessAccountPage}/>
+            <Route exact path="/notFound404" component={NotFound404}/>
+            <Redirect to="/notFound404" />
+          </Switch>
+        </UserContext.Provider>
       </ThemeProvider>
-      
     </Router>
   );
 }
--- src/components/AccountBox/Account.Api.js
+++ src/components/AccountBox/Account.Api.js
@@ -1,48 +1,71 @@
+import {useContext} from 'react';
 import {Auth} from 'aws-amplify';
+import {UserContext} from '../../utils/context/UserContext';
+import {useHistory} from 'react-router-dom';
+
 const AccountApi = () => {
-  
-  const PostSingupForm = async (data) => {
+  const {user, setUser} = useContext(UserContext);
+  const {history} = useHistory();
+
+  const AccountSignup = async (data) => {
     console.log('sign up:' ,data); //DELETEME
-    data.preventDefault();
+    // data.preventDefault(); // FIXME
+
+    let username = data.email  // TODO change to username?
+    let password = data.password
+    let email = data.email
+    try {
+      const { user } = await Auth.signUp({
+          username,
+          password,
+          attributes: {
+              email,
+          },
+      });
+      console.log(user);  //DELETEME
+    } catch (error) {
+      console.log('error signing up:', error);
+      return;
+  }
+
+  }
+
+  const AccountLogin = async (data) => {
+    console.log('login:' ,data);  // DELETEME
+    // data.preventDefault();  // FIXME
 
     try {
-      await Auth.signIn(userName, password)
+      await Auth.signIn(data.userName, data.password)
         .then(user => {
+          setUser(user);
+          console.log(user); //DELETEME
           alert("Logged in");
-          props.login(user);
           history.push("/myHome");
-          console.log(user.getUsername()); //DELETEME
         });
     } catch (e) {
       alert(e.message);
+    } 
+  }
+
+  const AccountLogout = async () => {
+    if(user == null) {
+      console.log('error: not logged-in log-out is impossible');
     }
+    
+    try {
+      await Auth.signOut()
+      .then(_ => {
+        setUser(null);
+        console.log('logout successfull');
+        alert("Logged out successfully");
+        history.push("/home");
+      })
+    } catch (e) {
+      alert('error logging out:', e);
+    }
+
   }
-
-  const PostLoginForm = async (data) => {
-    console.log('login:' ,data);  // DELETEME
-
-    data.preventDefault();
-        let username = email;
-        try {
-            const { user } = await Auth.signUp({
-                username,
-                password,
-                attributes: {
-                    email,
-                },
-            });
-            console.log(user);
-        } catch (error) {
-            console.log('error signing up:', error);
-            return;
-        }
-        console.log("Register succses email: " + email + "password: " + password);  // DELETEME
-        props.login(email);  // TODO signin with Auth.signIn and update userObject (instead of email)
-        history.push("/myHome")
-  }
-
-  return {PostSingupForm, PostLoginForm};
+  return {AccountSignup, AccountLogin, AccountLogout};
 }
-
 
 export default AccountApi;
